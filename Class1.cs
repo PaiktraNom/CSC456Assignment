2222024f13e5ee4ab1bed19bfa589739f28cf120
PaiktraNom <55154237+PaiktraNom@users.noreply.github.com>
2020-09-03 13:02:30 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nom1
+{
+    class Class1
+    {
+        public void Display()
+        {
+            string input = string.Empty;
+            do
+            {
+                Console.Write("Nom>");
+                input = Console.ReadLine();
+            } while (input != "exit");
+        }
+
+        static void Main(string [] args)
+        {
+            Class1 c = new Class1();
+            c.Display();
+        }
+    }
+}
