b29cd527f8e57f3a8f66b3ad9dadbf7e6fd605f5
PaiktraNom <55154237+PaiktraNom@users.noreply.github.com>
2020-09-09 14:34:47 -0500

    Add files via upload

--- Nom1/Class1.cs
+++ Nom1/Class1.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿/***************************************************
+ *  NAME:       PAIKTRA NOM
+ *  CLASS:      CSC 456
+ *  ASSIGNMENT: 1
+ *  DUE DATE:   9/11/2020
+ *  PROFESSOR:  PROFESSOR WON
+ *  OBJECTIVE:  IMPLEMENT A SIMPLE LINUX SHELL
+***************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Hosting;
 
 namespace Nom1
 {
@@ -18,39 +27,65 @@ namespace Nom1
             s.Run();
         }
     }
-    public class Shell
+    public class Shell  //Shell Class
     {
-        private Dictionary<string, string> Aliases = new Dictionary<string, string>()
-        {
-            {"ls", @"ListDirectories.exe" }
-        };
 
-        private List<string> args = new List<string>();
-        private string command = string.Empty;
-        public void Parse(string input)
+        private List<string> args = new List<string>(); //Arguments
+        private string command = string.Empty;          //Commmand
+        
+
+        /**********************
+         * FUNCTION NAME: Parse
+         * INPUT: string input
+         * OUTPUT: void
+         * DESCRIPTION: Parse command line input
+         **********************/
+        public void Parse(string input) //Get arguments
         {
+            args.Clear();
             char[] delimiterChars = { ' ', '\t' };
             string[] words = input.Split(delimiterChars);
             command = words[0];
             for(int i = 1; i < words.Length; i++)
             {
-                args.Append(words[i]);
+                args.Add(words[i]);
             }
         }
+
+        /**********************
+         * FUNCTION NAME: Run
+         * INPUT: None
+         * OUTPUT: void
+         * DESCRIPTION: Get command from user 
+         **********************/
         public void Run() 
         {
-            string input = string.Empty;
+            string input;
             do
             {
                 Console.Write("Nom>");
                 input = Console.ReadLine();
                 Parse(input);
-                Execute(input);
+                Execute(command);
             } while (input != "exit");
         }
         public void Execute(string input)
         {
-            ProcessStartInfo procStartInfo = new ProcessStartInfo("bash.exe", "-c " + input);
+            string argumentString = "-c " + "'" + input;
+            
+            if(args.Count > 0)
+            {
+                foreach(string arguments in args)
+                {
+                    argumentString += " " + arguments;
+                }
+                argumentString += "'";
+            }
+            else
+            {
+                argumentString += "'";
+            }
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("bash.exe", argumentString);
             procStartInfo.RedirectStandardOutput = true;
             procStartInfo.UseShellExecute = false;
             procStartInfo.CreateNoWindow = true;
@@ -61,6 +96,8 @@ namespace Nom1
             proc.Start();
 
             String result = proc.StandardOutput.ReadToEnd();
+            if (result == "")
+                result = proc.StandardError.ReadToEnd();
             Console.Write(result);
         }
     }
